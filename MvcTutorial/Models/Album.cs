34a3d269b7d4ca21767013c7abf6b89d03943fa5
Apata Oluwaseun <apata4lyfe@live.com>
2016-08-30 01:34:43 +0100

    Ok

---
+++ MvcTutorial/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MvcTutorial.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public  string  Title { get; set; }
+        public Artist Artist { get; set; }
+
+        public virtual List<Review> Review { get; set; }
+    }
+}
---
+++ MvcTutorial/Models/Review.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcTutorial.Models
+{
+    public class Review
+    {
+        public int Reviewid { get; set; }
+
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+
+
+        public string Contents { get; set; }
+
+        [Required()]
+        [Display(Name ="Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}
